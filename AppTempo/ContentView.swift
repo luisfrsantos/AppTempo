aaebf6b757c605f25450d61da11875b5083d8b64
Luis Fernando <luisbluex@gmail.com>
2024-02-15 18:11:49 -0300

    first commit

--- AppTempo/ContentView.swift
+++ AppTempo/ContentView.swift
@@ -10,11 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("25°C")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 42))
+                    .bold()
+                    
+            }
+            .background(Color.white)
+            .cornerRadius(6)
+            .padding()
+          
         }
+        .background(Color.blue)
         .padding()
     }
 }
