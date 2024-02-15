424f24d1222810812fc5834db9e75ad099b6bcd6
Luis Fernando <luisbluex@gmail.com>
2024-02-15 16:11:28 -0300

    Initial Commit

---
+++ AppTempo/AppTempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTempoApp.swift
+//  AppTempo
+//
+//  Created by MAC on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AppTempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
