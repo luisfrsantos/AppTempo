8f8ceb00597195de618939c7aa10551a721bb7aa
Luis Fernando <luisbluex@gmail.com>
2024-02-15 22:58:51 -0300

    Create time component

--- AppTempo/ContentView.swift
+++ AppTempo/ContentView.swift
@@ -9,21 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            HStack {
-                Text("25°C")
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    .font(.system(size: 42))
-                    .bold()
-                    
-            }
-            .background(Color.white)
-            .cornerRadius(6)
-            .padding()
-          
-        }
-        .background(Color.blue)
-        .padding()
+            VStack {
+                VStack {
+                    Text("São paulo")
+                        .font(.system(size: 18))
+                        .foregroundColor(.blue)
+                        .bold()
+                        .padding(EdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 0))
+                    HStack {
+                        Text("25°C")
+                            .foregroundColor(.blue)
+                            .font(.system(size: 42))
+                            .bold()
+                        
+                        Image("daylight", label: Text("Sol"))
+                    }.padding(EdgeInsets(top: 0, leading: 16, bottom: 16, trailing: 16))
+                }
+                .background(Color.white)
+                .cornerRadius(6)
+                .padding()
+            }.frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity,
+                    alignment: .top
+                )
+            
+        }.background(Color.blue)
+        
     }
 }
 
